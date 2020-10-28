c200ed0c0f56d593f843bbb962c0a875c10ffed4
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2020-10-28 15:41:32 +0200

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -1,10 +1,10 @@
 function order(phrase) {
  let res = phrase
-    .split(" ")
-    .sort(function(a, b) {
-      return a.match(/\d/) - b.match(/\d/);
+    .split(' ')
+    .sort(function(a , b) {
+      return a.match( /\d/ ) - b.match( /\d/ );
     })
-    .join(" ");
+    .join(' ');
 alert(res)
 }
 order('is2 Thi1s T4est 3a')
